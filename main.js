b2c1548d4076f026ea475c1ef44e9195cab7d84c
comsaziz <comsaziz@gmail.com>
2024-09-08 14:40:05 +0300

    Modifiy

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ var vessel = "cone";
 
  if((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel ===  "bowl") && (toppings === "sprinkles" || toppings === "peanuts")){
     console.log( "I'd like two scoops of" + flavor + "ice cream in a"+ vessel +"with"+ toppings +"")
- }
+ }else console.log("Thank you");
 
  
  
